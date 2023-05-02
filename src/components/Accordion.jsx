c65953c68605ba7431e004f04178b7e5c1a3c630
Javiersandoval1 <ja.sandovalj@duocuc.cl>
2023-05-02 17:23:50 -0400

    rrr

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -7,7 +7,7 @@ const { definePartsStyle, defineMultiStyleConfig } =
 const baseStyle = definePartsStyle({
   // define the part you're going to style
   container: {
-    bg: '#0EA293', // change the backgroundColor of the container
+    bg: '#0EA293', // change the backgroundColor of the containerrrrs
     borderRadius: '8px',
     margin: '8px'
   },
