2f19bf95d599bc8a7ff49f6338dc5c67118dbdf9
CristianIlic <cris.ilic96@gmail.com>
2023-10-24 19:42:51 -0300

    configuring vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        /^@fontsource\/Poppins\/600\.css$/,
+        // Agrega otras dependencias externas si es necesario
+      ],
+    },
+  },
+});
