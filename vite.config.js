ec848b1657646bdc2755ccc794b5c55eda51b24e
CristianIlic <cris.ilic96@gmail.com>
2023-10-25 15:58:05 -0300

    intento fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@fontsource": "./node_modules",
+      "@fontsource": "/node_modules/@fontsource",
     }
   }
 })
