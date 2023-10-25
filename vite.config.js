f1209a3ac8f88c493f59de651b436060669a8112
CristianIlic <cris.ilic96@gmail.com>
2023-10-25 16:15:21 -0300

    fix tryu

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ViteCSSPlugin from 'vite-plugin-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),ViteCSSPlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@fontsource/Poppins': '@fontsource/poppins',
